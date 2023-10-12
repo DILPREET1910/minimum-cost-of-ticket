5b2e7830655d003f99c52724630726c3df9b8e97
DILPREET1910 <dsahney1910@gmail.com>
2023-10-12 17:17:15 +0530

    date in path instead of day of year

--- lib/services/minimumCostOfTicket.dart
+++ lib/services/minimumCostOfTicket.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart';
+
 // lib imports
 import 'package:ads_course_project_minimum_cost_of_ticket/services/globalVariables.dart' as global;
 import 'package:ads_course_project_minimum_cost_of_ticket/services/node.dart';
@@ -18,14 +20,15 @@ class MinimumCost {
 
     if (currentOneDayIndex == -1) {
       Node temp = createNode(367, root.amount + costs[0],
-          "${root.path}\nOne Day pass at day ${days[currentDayIndex]}");
+          "${root.path}\nOne Day pass at day ${DateFormat("dd-MMM-yy").format(DateTime(DateTime.now().year, 1, days[currentDayIndex]))}");
       endNodes.add(temp);
       return temp;
     }
 
     root.dayPassNode = createNode(days[currentOneDayIndex], 0, "");
     root.dayPassNode!.amount = root.amount + costs[0];
-    root.dayPassNode!.path = "${root.path}\nOne Day pass at day ${days[currentDayIndex]}";
+    root.dayPassNode!.path =
+        "${root.path}\nOne Day pass at day ${DateFormat("dd-MMM-yyyy").format(DateTime(DateTime.now().year, 1, days[currentDayIndex]))}";
     Node newNode = root.dayPassNode!;
     newNode.dayPassNode = addOneDayPassNode(days, costs, currentOneDayIndex, newNode);
     newNode.sevenDayPassNode = addSevenDayPassNode(days, costs, currentOneDayIndex, newNode);
@@ -38,14 +41,15 @@ class MinimumCost {
 
     if (currentSevenDayIndex == -1) {
       Node temp = createNode(367, root.amount + costs[1],
-          "${root.path}\nSeven Day pass at day ${days[currentDayIndex]}");
+          "${root.path}\nSeven Day pass at day ${DateFormat("dd-MMM-yyyy").format(DateTime(DateTime.now().year, 1, days[currentDayIndex]))}");
       endNodes.add(temp);
       return temp;
     }
 
     root.sevenDayPassNode = createNode(days[currentSevenDayIndex], 0, "");
     root.sevenDayPassNode!.amount = root.amount + costs[1];
-    root.sevenDayPassNode!.path = "${root.path}\nSeven Day pass at day ${days[currentDayIndex]}";
+    root.sevenDayPassNode!.path =
+        "${root.path}\nSeven Day pass at day ${DateFormat("dd-MMM-yyyy").format(DateTime(DateTime.now().year, 1, days[currentDayIndex]))}";
     Node newNode = root.sevenDayPassNode!;
     newNode.dayPassNode = addOneDayPassNode(days, costs, currentSevenDayIndex, newNode);
     newNode.sevenDayPassNode = addSevenDayPassNode(days, costs, currentSevenDayIndex, newNode);
@@ -58,14 +62,15 @@ class MinimumCost {
 
     if (currentThirtyDayIndex == -1) {
       Node temp = createNode(367, root.amount + costs[2],
-          "${root.path}\nThirty Day pass at day ${days[currentDayIndex]}");
+          "${root.path}\nThirty Day pass at day ${DateFormat("dd-MMM-yyyy").format(DateTime(DateTime.now().year, 1, days[currentDayIndex]))}");
       endNodes.add(temp);
       return temp;
     }
 
     root.thirtyDayPassNode = createNode(days[currentThirtyDayIndex], 0, "");
     root.thirtyDayPassNode!.amount = root.amount + costs[2];
-    root.thirtyDayPassNode!.path = "${root.path}\nThirty Day pass at day ${days[currentDayIndex]}";
+    root.thirtyDayPassNode!.path =
+        "${root.path}\nThirty Day pass at day ${DateFormat("dd-MMM-yyyy").format(DateTime(DateTime.now().year, 1, days[currentDayIndex]))}";
     Node newNode = root.thirtyDayPassNode!;
     newNode.dayPassNode = addOneDayPassNode(days, costs, currentThirtyDayIndex, newNode);
     newNode.sevenDayPassNode = addSevenDayPassNode(days, costs, currentThirtyDayIndex, newNode);
